67c1152d54b5e82f273ca0b0ff14226ac73a64ed
tarique313 <mahmud.tarique@gmail.com>
2017-02-09 14:12:30 +0600

    Fix routing errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+          :recoverable, :rememberable, :trackable, :validatable
           #:confirmable
   #include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
 
+  scope "(:locale)", locale: /en|bn/ do
+    root 'home#index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, controllers: {registrations: "registrations", omniauth_callbacks: "users/omniauth_callbacks"}
-  scope "(:locale)", locale: /en|bn/ do
+  devise_for :users, controllers: {registrations: "registrations"}, skip: :omniauth_callbacks
 
-  root 'home#index'
+  scope "(:locale)", locale: /en|bn/ do
+    resources :users do
+      member do
+        get :orders
+      end
+    end
+    resources :orders do
+      resources :comments
+    end
   #get 'accounts/index'
 
   resources "contacts", only: [:new, :create]
@@ -15,15 +24,9 @@ Rails.application.routes.draw do
   get 'faq/index'
   get 'user_accounts/index', as: "accounts"
 
-  resources :orders do
-    resources :comments
-  end
+
+
  end
-  resources :users do
-    member do
-      get :orders
-    end
-  
-end
   match '/contacts', to: 'contacts#new', via: 'get'
-end
+  end
+  end
